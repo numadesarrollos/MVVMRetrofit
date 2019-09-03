4af55ac4e5afda22437b55e2851d6f6aa270716c
Borja <bau.isban@gmail.com>
2019-09-04 00:00:11 +0200

    Setting API_KEY and BASE_URL

---
+++ app/src/main/java/com/numadesarrollos/mvvmrecepy/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.numadesarrollos.mvvmrecepy.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://www.fod2fork.com";
+    public static final String API_KEY = "12c630aac9586bd3bff51b1db536b5d6";
+}
